f1401adefe7b6b05c8c591211597744b9a8f08a1
sivelswhy <82051273+sivelswhy@users.noreply.github.com>
2022-01-30 16:56:18 +0100

    Added Google Analytics

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
 }
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-3S62TTDK8Q');
